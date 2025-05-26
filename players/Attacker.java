39369c532a0f5c56072490424a48d43fe4f695a2
Richard Tuo <rickiebieni@gmail.com>
2025-05-26 18:03:12 -0400

    Bug fixes and added getters to the player list

--- players/Attacker.java
+++ players/Attacker.java
@@ -1,1 +1,31 @@
 package players;
+
+import becker.robots.City;
+import becker.robots.Direction;
+
+public class Attacker extends Player{
+
+	public Attacker(City city, int s, int a, Direction d) {
+		super(city, s, a, d);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void performAction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected Direction getNextDirection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected int getNextMovement() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
--- players/Player.java
+++ players/Player.java
@@ -11,7 +11,7 @@ public abstract class Player extends EnhancedBot{
 	private static int nextID = 0; // Next playerID of next created player; corresponds with index of playerList
 	private static List<PlayerRecord> playerList = new ArrayList<PlayerRecord>();
 	
-	private int playerID; // Used to identify player - unique to each Player object
+	protected int playerID; // Used to identify player - unique to each Player object
 	
 	/**
 	 * Constructor of player
@@ -55,6 +55,32 @@ public abstract class Player extends EnhancedBot{
 			Player.nextID++; // Iterates playerID to create a unique player identification number
 		}
 	}
+	
+	/**
+	 * Getter for PlayerRecord in the ArrayList
+	 * @param playerID - ID of the player
+	 * @return - PlayerRecord ID references
+	 */
+	public static PlayerRecord getPlayerRecord(int playerID){
+		// Checks if index is out of bounds or not
+		if (playerID >= 0 && playerID < playerList.size()) {
+			return playerList.get(playerID);
+		} else {
+			System.out.println("ERROR");
+			return new PlayerRecord(null, 0, 0);
+		}
+	}
+	
+	/**
+	 * Getter for the player list
+	 * pre: accessed by other player classes
+	 * @return - the list of player records
+	 */
+	protected static List<PlayerRecord> getPlayerList(){
+		return Player.playerList;
+	}
+	
+	
 	
 	/**
 	 * Gets the next step's direction 
