d8a6025bf27639649c481ddba30f978e9a5b5dc7
unknown <184700092+Fewixi@users.noreply.github.com>
2025-05-26 19:42:40 -0400

    added a map

--- app/Main.java
+++ app/Main.java
@@ -10,6 +10,21 @@ import becker.robots.*;
 public class Main {
 	public static void main(String[] args) {
 		City city = new City();
+		
+		Wall [][] walls = new Wall[24][13];
+
+		// creates horizontal walls
+		for (int i = 0; i < walls.length; i ++) {
+			walls[i][0] = new Wall(city, 0, i, Direction.NORTH);
+			walls[i][walls[0].length - 1] = new Wall(city, walls[0].length - 1, i, Direction.SOUTH);
+		}
+
+		// creates vertical walls
+		for (int i = 0; i < walls[0].length; i ++) {
+			walls[0][i] = new Wall(city, i, 0, Direction.WEST);
+			walls[walls.length - 1][i] = new Wall(city, i, walls.length - 1, Direction.EAST);
+		}
+		
 		Attacker attacker = new Attacker(city, 0, 0, Direction.EAST);
 		String nameOfClass = Player.getPlayerRecord(0).getPlayer().getClass().getSimpleName();
 		System.out.println(nameOfClass);
