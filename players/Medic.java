c49f58e16a7a9f5a4e6eea661fbc65c8699f23d9
Richard Tuo <rickiebieni@gmail.com>
2025-05-29 14:05:58 -0400

    temp

--- players/Medic.java
+++ players/Medic.java
@@ -1,15 +1,21 @@
 package players;
+import java.awt.Color;
 import becker.robots.*;
 
 public class Medic extends Player{
 	public Medic (City c, int s, int a, Direction d) {
 		super(c, s, a, d, 3, 4, "Medic");
+		this.setColor(new Color(133, 248, 108));
 	}
 
 	@Override
 	protected void sortPriority() {
-		// TODO Auto-generated method stub
-		
+		PlayerRecord[] newList = this.priorityList;
+		for (int i = 0; i < newList.length; i++) {
+			for (int j = i; i < newList.length; i++) {
+				if ()
+			}
+		}
 	}
 
 	@Override
