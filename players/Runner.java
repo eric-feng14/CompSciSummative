d3fd5013090ddb87b43d1dfeb776692039b84ea9
unknown <184700092+Fewixi@users.noreply.github.com>
2025-05-31 19:54:45 -0400

    update runner

--- players/Runner.java
+++ players/Runner.java
@@ -3,20 +3,22 @@ import java.awt.Color;
 import becker.robots.*;
 
 public class Runner extends Player{
-	
+
 	private boolean isCaught = false;
 	private int agility;
-	
+
 	public Runner(City c, int s, int a, Direction d) {
 		super(c, s, a, d, 5, 5, "Runner");
 		this.setColor(Color.BLUE);
+		this.agility = 5;
 	}
 
 	@Override
 	protected void sortPriority() {
 		for (int i = 1; i < priorityList.length; i ++) {
 			for (int j = i; j > 0; j--) {
-				if ((priorityList[i].getTYPE().compareTo(priorityList[j - 1].getTYPE()) < 0) || ) {
+				if ((priorityList[i].getTYPE().compareTo(priorityList[j - 1].getTYPE()) < 0) 
+						&& this.calcDistance(priorityList[i]) < this.calcDistance(priorityList[i-1])) {
 					PlayerRecord record = priorityList[j];
 					priorityList[j] = priorityList[j - 1];
 					priorityList[j - 1] = record;
@@ -27,10 +29,10 @@ public class Runner extends Player{
 			}
 		}
 	}
-	private int calculateDistance(PlayerRecord r) { 
+	private int calcDistance(PlayerRecord r) { 
 		return Math.abs(r.getAvenue() - this.getAvenue()) + Math.abs(r.getStreet() - this.getStreet());
 	}
-	
+
 	@Override
 	protected void performAction() {
 		
